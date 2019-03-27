e9fcf6843452156bea0cc79d0b9385554b741aa4
Jens Nevens <jnevens@vub.ac.be>
2019-03-27 15:42:56 +0100

    git subrepo clone gitlab@gitlab.ai.vub.ac.be:ehai/experiment-framework.git experiment-framework
    subrepo:
      subdir:   "experiment-framework"
      merged:   "641abdd"
    upstream:
      origin:   "gitlab@gitlab.ai.vub.ac.be:ehai/experiment-framework.git"
      branch:   "master"
      commit:   "641abdd"
    git-subrepo:
      version:  "0.4.0"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "5d6aba9"

---
+++ experiment-framework/agent.lisp
@@ -0,0 +1,72 @@
+
+(in-package :experiment-framework)
+
+;; ############################################################################
+;; agent
+;; ----------------------------------------------------------------------------
+
+(export '(agent
+          discourse-role utterance
+          communicated-successfully
+          speakerp hearerp))
+
+(defclass agent (configuration blackboard)
+  ((experiment :initarg :experiment :accessor experiment :initform nil
+               :documentation "The experiment this agent is part of")
+   (id
+    :documentation "The unique identifier of this agent."
+    :type (or symbol fixnum) :initarg :id :initform (make-id "AGENT") :reader id)    
+   (discourse-role 
+    :documentation "A symbol designating the role the agent plays in
+    the game. e.g. unknown, speaker, hearer. You have to set this slot
+    yourself in method determine-interacting-agents."
+    :type symbol :initform 'not-set :accessor discourse-role)
+   (utterance 
+    :documentation "The utterance that either a speaker produced or a
+    hearer received. You have to set this slot yourself."
+    :type t :initarg :utterance :initform nil :accessor utterance)
+   (communicated-successfully 
+    :documentation "Whether the agent experienced success."
+    :initform nil :accessor communicated-successfully))
+  (:documentation "Base class for all agents"))
+
+;; ----------------------------------------------------------------------------
+;; speakerp and hearerp
+
+(defgeneric speakerp (agent)
+  (:documentation "Predicate to check if the agent is the speaker"))
+
+(defmethod speakerp ((agent agent))
+  (eql (discourse-role agent) 'speaker))
+
+(defgeneric hearerp (agent)
+  (:documentation "Predicate to check if the agent is the hearer"))
+
+(defmethod hearerp ((agent agent))
+  (eql (discourse-role agent) 'hearer))
+
+;; ----------------------------------------------------------------------------
+;; get-configuration
+
+(defmethod get-configuration ((agent agent) key &key &allow-other-keys)
+  "get-configuration for a agent tries the experiment if no configuration
+   is found in the agent"
+  (multiple-value-bind (entry found)
+      (call-next-method)
+    (if found
+      (values entry found)
+      (get-configuration (experiment agent) key))))
+
+;; ----------------------------------------------------------------------------
+;; print-object
+
+(defmethod print-object ((agent agent) stream)
+  (if *print-pretty*
+    (pprint-logical-block (stream nil)
+      (format stream "<~(~:w~):~:_ id: ~a,~:_ configuration: ~a,~:_" 
+              (type-of agent) (id agent) (configuration agent))
+      (call-next-method)
+      (format stream "~:_ data: ~a>" (data agent)))
+    (format stream "<~(~:w~) ~a>" (type-of agent) (id agent))))
+
+
